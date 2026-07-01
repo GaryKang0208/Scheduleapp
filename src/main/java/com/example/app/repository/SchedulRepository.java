c9ae72f3713d3d931263bcf0ca4738d9d7392e78
GaryKang0208 <khb1661@gmail.com>
2026-07-01 16:03:43 +0900

    Lv 0

--- src/main/java/com/example/app/repository/SchedulRepository.java
+++ src/main/java/com/example/app/repository/SchedulRepository.java
@@ -3,5 +3,9 @@ package com.example.app.repository;
 import com.example.app.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SchedulRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findAllByAuthorOrderByModifiedAt(String author);
 }
